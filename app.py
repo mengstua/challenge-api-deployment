2e973377d0151efeb43f19984bccc27273c17a73
nadiya0509 <n27032001@yahoo.com>
2025-07-08 22:08:45 +0200

    Intoduced methods to handle status 500 and 422 errors (to validate POST JSON input)

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Request
+import logging
+from fastapi import FastAPI, Request, requests
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from typing import Optional, Literal
+from pydantic import BaseModel, Field
 import pandas as pd
 import os
+
+from pydantic import BaseModel
 
 from prepocessing.preprocessing import Immo_Preprocessing #####!!!!! spelling error: preposessing
 from predict.predict_Nad import Predict_Nad
@@ -12,10 +16,47 @@ from predict.predict_Nad import Predict_Nad
 
 #app = FastAPI()
 
+
 # Set port to the env variable PORT to make it easy to choose the port on the server
 # If the Port env variable is not set, use port 8000
 PORT = os.environ.get("PORT", 8000)
 app = FastAPI(port=PORT)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("myapp.log")
+logger.addHandler(file_handler)
+
+################## To handle errors with incorrectly input variables in the POST method
+
+# Middleware to catch and log all unhandled exceptions (like 500 errors)
+@app.middleware("http")
+async def log_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as exc:
+        logger.error(  # Prints in the log file
+            "Unhandled exception encountered",
+            exc_info=True,  # This writes the full traceback to the log file
+            # extra={
+            #     "path": request.url.path,
+            #     "method": request.method,
+            #     "client_host": request.client.host
+            # }
+        )
+        # Print on screen
+        return JSONResponse(
+            status_code=500,
+            content={"500 error detail ": "Internal Server Error"}
+        )
+     
+##########################""    
+    
+@app.get("/check_LoggingStatus")
+async def root():
+    logger.info("Hello, logging works!")
+    return {"message": "Look for the message above in the .log file!"}
+
 
 @app.get("/")
 async def root():
@@ -31,7 +72,7 @@ async def predict_info():
 data_format_json =  { #  TO DO: adjust str, bool, etc according to my comments
   "Format of your JSON input for POST requests:": {
     "area": "int",
-    "property-type": "APARTMENT | HOUSE ",     # Removed | OTHERS - not in our model!   
+    "property-type": "APARTMENT | HOUSE",     # Removed | OTHERS - not in our model!   
     "rooms-number": "int",
     "zip-code": "int",
     " land-area (optional)": "int",  # If absent- will be replaced with 0.
@@ -58,22 +99,126 @@ data_format_json =  { #  TO DO: adjust str, bool, etc according to my comments
   }
 }
 
-################## To handle errors with incorrectly input variables in the POST method
+################## Will result from errors in http://127.0.0.1:8000/predict_WithBetterValidation
 
-@app.exception_handler(500) # E.g; "garden" = "FALSE"
-async def internal_exception_handler(request: Request, exc: Exception):
-    errors = exc.errors()
+@app.exception_handler(RequestValidationError) 
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors=exc.args[0]
     formatted_errors = []
-    for error in errors:
-      custom_message = generate_custom_message(error)
-      formatted_errors.append({custom_message})
-    def generate_custom_message(error):
-        return f"Internal Server Error. Check field {error['loc']}. "  
-    return JSONResponse(
-        JSONResponse(status_code=500, content={"errors": f"{formatted_errors}"})
-    )
 
-#################  Process input JSON file
+    def generate_custom_message(error, field):
+        # Customize messages based on error type
+        if error["type"] == "missing":
+            return f" ---You forgot to input: {field}."
+        # Add more cases as needed
+        return f" ---Wrong type of input for: {field} - {error["msg"]}"  
+          
+    for error in errors:
+      field = error["loc"]
+      custom_message = generate_custom_message(error, field)
+      formatted_errors.append(custom_message)
+    
+    return JSONResponse(
+        status_code=422,
+        #content=jsonable_encoder({"errors": formatted_errors, "body": exc.body, "Detail": exc.args}),
+        content=jsonable_encoder({"Errors": formatted_errors}),
+    )
+#################  Process input JSON file with validation of types ####################
+
+# Needed for validation of input data and renaming is needed because can't have dashes in the variables' names
+class PropertyData(BaseModel):
+    area: int
+    property_type: Literal["APARTMENT", "HOUSE"] = Field(..., alias="property-type")
+    rooms_number: int = Field(..., alias="rooms-number")
+    zip_code: int = Field(..., alias="zip-code")
+    land_area: Optional[int]  = Field(None, alias="land-area")
+    garden: Optional[bool] = None
+    garden_area: Optional[int] = Field(None, alias="garden-area")
+    equipped_kitchen: Optional[bool] = Field(None, alias="equipped-kitchen")
+    full_address: Optional[str] = Field(None, alias="full-address")
+    swimming_pool: Optional[bool] = Field(None, alias="swimming-pool") 
+    furnished: Optional[bool] = None
+    open_fire: Optional[bool] = Field(None, alias="open-fire")
+    terrace: Optional[bool] = None
+    terrace_area: Optional[int] = Field(None, alias="terrace-area")
+    facades_number: Optional[int] = Field(None, alias="facades-number")
+    building_state: Optional[Literal["NEW", "GOOD", "TO RENOVATE", "JUST RENOVATED", "TO REBUILD"]]  = Field(None, alias="building-state")
+    lift: int
+    parkingcountindoor: int
+    parkingcountoutdoor: int
+    province: Literal['Antwerp', 'East Flanders','Flemish Brabant', 'Limburg','West Flanders',
+                      'Walloon Brabant', 'Hainaut', 'Liège', 'Luxembourg', 'Namur', 'Brussels']
+    epcscore: Literal['A++', 'A+', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    class Config:
+        allow_population_by_field_name = True
+
+@app.post("/predict_WithBetterValidation")
+async def predict_WithBetterValidation(propertyData: PropertyData):
+
+  data_dict = propertyData.model_dump()
+
+  df = pd.DataFrame([data_dict])
+
+  df["price"] = 0
+  df["subtype"] = "subtype"
+  df["locality"] = "locality"
+
+  # Drop columns according to preprocessing.py
+  df.drop(columns=['garden_area', 'equipped_kitchen', 'full_address',
+                    'furnished', 'open_fire', 'terrace_area','facades_number'], inplace=True, errors='ignore')
+  
+  # Fix column names
+  df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+  # Rename columns according to preprocessing.py
+
+  if 'building_state' not in df.columns: df['building_state'] = "GOOD"
+  if 'terrace' not in df.columns: df['terrace'] = 0
+  if 'swimming_pool' not in df.columns: df['swimming_pool'] = 0
+  if 'garden' not in df.columns: df['garden'] = 0
+  if 'land_area' not in df.columns: df['land_area'] = 0
+
+  df.rename(columns={ 'rooms_number': 'bedroomcount',      
+                              'area': 'habitablesurface',
+                     'property_type': 'type',
+                          'zip_code': 'postcode',
+                         'land_area': 'landsurface',
+                            'garden': 'hasgarden',
+                     'swimming_pool': 'hasswimmingpool',
+                           'terrace': 'hasterrace',
+                    'building_state': 'buildingcondition',
+                              'lift': 'haslift'
+                        }, inplace=True)
+  
+  df_temp = df
+  immo_Preprocessing = Immo_Preprocessing("data_cleaned_Nad.csv") # We don't need the csv, but because I don't want to modify preprocessing.py, I need to give it some file.
+  immo_Preprocessing.df=df_temp # Here we replace df that was before populated from the csv to train the model, with our json input.
+
+  immo_Preprocessing.fill_boolean_values()
+  immo_Preprocessing.add_parking_col()
+  immo_Preprocessing.add_region_column()
+  #immo_Preprocessing.eliminate_outliers()
+  #immo_Preprocessing.eliminate_postcode_small_number()
+  immo_Preprocessing.drop_remaining_rows()
+  immo_Preprocessing.oe_ohe_processing()
+  immo_Preprocessing.drop_remaining_columns()
+  immo_Preprocessing.drop_duplicates()
+  immo_Preprocessing.latitude_longitude_columns('prepocessing//data//georef-belgium-postal-codes@public.csv')
+  # Create missing region columns
+  columns_to_check = ['region_Brussels', 'region_Flanders', 'region_Wallonia']
+  for col in columns_to_check:
+    if col not in immo_Preprocessing.df.columns:
+       immo_Preprocessing.df[col] = 0
+
+  #*** Caterina- did you normalized your df before doing modeling? If yes, we need to normalise our input then! But what are norm. coeff-s???
+
+  price_pred=Predict_Nad.predict_price(immo_Preprocessing.df)
+
+  print("Predicted price: ", price_pred)
+  return(JSONResponse(status_code=200, content= {"price_pred" : price_pred}))
+
+
+#################  Process input JSON file with less validation
 
 @app.post("/predict")
 async def predict(request: Request):
@@ -112,8 +257,6 @@ async def predict(request: Request):
                               'lift': 'haslift'
                         }, inplace=True)
   
-  print ("Ok now!!!!!!!!!!!")
-  
   df_temp = df
   immo_Preprocessing = Immo_Preprocessing("data_cleaned_Nad.csv") # We don't need the csv, but because I don't want to modify preprocessing.py, I need to give it some file.
   immo_Preprocessing.df=df_temp # Here we replace df that was before populated from the csv to train the model, with our json input.
@@ -134,11 +277,11 @@ async def predict(request: Request):
     if col not in immo_Preprocessing.df.columns:
        immo_Preprocessing.df[col] = 0
 
-  ############ Caterina- did you normalized your df before doing modeling? If yes, we need to normalise our input then! But what are norm. coeff-s???
+  #*** Caterina- did you normalized your df before doing modeling? If yes, we need to normalise our input then! But what are norm. coeff-s???
 
   price_pred=Predict_Nad.predict_price(immo_Preprocessing.df)
 
   print("Predicted price: ", price_pred)
-  return{"price_pred" : price_pred}
+  return(JSONResponse(status_code=200, content= {"price_pred" : price_pred}))
 
 
